73e0ad89a2b720db526f01429bb7f03d80ccb801
Nikolaj Veljkovic <nikjaspamm@gmail.com>
2025-08-06 20:45:10 +0200

    feat: user-friendly shell updates.

--- goodreads-to-obsidian.sh
+++ goodreads-to-obsidian.sh
@@ -20,6 +20,7 @@ month=$(date +%B) # Mon
 # grabs title, cover image, author name, publishing year and book id
 # from 'currently reading' RSS feed and removes all HTML and tabs
 # sed syntax: sed -e 's/contenttoreplace/contenttoinsert/'
+echo "Getting 'Currently Reading' data."
 IFS=$'\n' readingfeed=$(curl --silent "$readingurl" | \
 egrep 'title|book_large_image_url|author_name|book_published|book_id' | \
 sed -e 's/<!\[CDATA\[//' -e 's/\]\]>//' \
@@ -35,6 +36,7 @@ fmt -u # uniform spacing
 )
 
 # grabs book id from 'read' RSS feed and removes all HTML and tabs
+echo "Getting 'Read' data."
 IFS=$'\n' readfeed=$(curl --silent "$readurl" | egrep 'book_id' | \
 sed -e 's/<book_id>//' -e 's/<\/book_id>/ | /' \
 -e 's/^[ \t]*//' -e 's/[ \t]*$//' | \
@@ -42,6 +44,7 @@ fmt -u # uniform spacing
 )
 
 # turns the data into an array, by substituting '|' for a new-line character
+echo "Putting the data into an array."
 readingarr=($(echo $readingfeed | tr "|" "\n")) # outer pair of brackets is necessary for array definition
 readarr=($(echo $readfeed | tr "|" "\n"))
 
@@ -58,7 +61,8 @@ done
 # gets the amount of books by dividing array by 5
 readingamount=$((${#readingarr[@]} / 5))
 
-# checks if book is in directory
+# checks if book is in directory, otherwise removes it
+echo "Removing books that already have a note:"
 for (( i = 0 ; i < ${readingamount} ; i++ ))
 do
   # temporary counter variable
@@ -71,17 +75,18 @@ do
   # grep scans all notes in vaultpath for an appearance of bookid
   if grep -q "${bookid}" -r "${vaultpath}"; then
     # removes the book from the array
+    echo "  '${readingarr[$counter]}'"
     unset readingarr[$counter]
     unset readingarr[$(($counter + 1))]
     unset readingarr[$(($counter + 2))]
     unset readingarr[$(($counter + 3))]
     unset readingarr[$(($counter + 4))]
-    echo "Book '${readingarr[$counter]}' already exists"
   fi
 done
 
 # readingarr now might have gaps, because of unset values
 # creates an updated array with no gaps
+echo "Cleaning up the array."
 for i in "${!readingarr[@]}"
 do
     new_array+=("${readingarr[i]}")
@@ -94,8 +99,10 @@ readingamount=$((${#readingarr[@]} / 5))
 
 if (("$readingamount" == 0)); then
   echo "Currently Reading: No new books found."
+  echo
 else
-  echo "Starting Process..."
+  echo "--- Starting Process ---"
+  echo
 fi
 
 # creates a note for each book
@@ -121,6 +128,7 @@ do
   # writes the contents for the book file
   if [[ "$cleantitle" == "" ]]; then
     echo "Error! Failed to create note due to faulty title."
+    echo
   else
     echo "---
 bookid: '${bookid}'
@@ -140,21 +148,27 @@ Link to reference:
 " >> "${vaultpath}/${cleantitle}.md"
     # displays a notification when file was created
     echo "Booknote created with title '${cleantitle}'"
+    echo
   fi
 done
 
 # if a book was read, change the tag and add read date
+echo "--- Updating read books ---"
 updatecounter=0
 for i in ${!readarr[@]}
 do
   # return path of book with matching bookid
-  readbookpath=$(find "${vaultpath}" -type f -print0 | xargs -0 grep -li "bookid: \"${readarr[$i]}\"")
+  readbookpath=$(find "${vaultpath}" -type f -print0 | xargs -0 grep -li "bookid: '${readarr[$i]}'")
 
+  # add Year read and replace #currently-reading with #read
   if [ "$readbookpath" != "" ]; then
-    year=$(date +%Y)
-    sed -i -e '/Year published: [0-9][0-9][0-9][0-9]/a Year read: ${year}' "$readbookpath"
-    sed -i -e 's/#currently-reading/#read/' "$readbookpath"
-    ((updatecounter++))
+    # if the read book was already marked as read, skip to the next book
+    if [ $(echo $(grep -ci "#read" "$readbookpath")) == "0" ]; then
+      year=$(date +%Y)
+      sed -i -e '/Year published: [0-9][0-9][0-9][0-9]/a Year read: ${year}' "$readbookpath"
+      sed -i -e 's/#currently-reading/#read/' "$readbookpath"
+      ((updatecounter++))
+    fi
   fi
 done
 
